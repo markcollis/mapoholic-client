5a7923d8e20500bad9724ac259fbafe8c3771c88
Mark Collis <mark@markandblanka.com>
2019-09-17 14:47:51 +0200

    More translations

--- src/components/home/HomeAdminLogging.js
+++ src/components/home/HomeAdminLogging.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Trans } from '@lingui/macro';
 
 import { setApiLoggingAction } from '../../actions';
 
@@ -19,9 +20,13 @@ class HomeAdminLogging extends Component {
   render() {
     const { currentUserRole, logApiCalls } = this.props;
     const isAdmin = (currentUserRole === 'admin');
-    const messageText = `API logging is currently ${(logApiCalls) ? 'enabled' : 'disabled'}.`;
+    const messageText = (logApiCalls)
+      ? <Trans>API logging is currently enabled.</Trans>
+      : <Trans>API logging is currently disabled.</Trans>;
     const messageClass = (logApiCalls) ? 'ui compact message positive' : 'ui compact message negative';
-    const buttonText = (logApiCalls) ? 'Disable logging' : 'Enable logging';
+    const buttonText = (logApiCalls)
+      ? <Trans>Disable logging</Trans>
+      : <Trans>Enable logging</Trans>;
     const buttonClass = (logApiCalls) ? 'ui button primary' : 'ui button primary';
     return (
       <div className="home-admin-logging">
