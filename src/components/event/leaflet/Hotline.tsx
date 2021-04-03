20eef280a1f8da8a29240a2b9888785836ead6cb
Mark Collis <markc@salsitasoft.com>
2021-04-03 07:02:07 +0200

    Initial test

---
+++ src/components/event/leaflet/Hotline.tsx
@@ -0,0 +1,88 @@
+/* eslint-disable no-underscore-dangle */
+import 'leaflet-hotline';
+import * as L from 'leaflet';
+import { LayerProps, LeafletContextInterface, createLayerComponent } from '@react-leaflet/core';
+import { useEffect } from 'react';
+
+// const DEFAULT_OPTIONS = {
+//   min: ?
+//   max: ?
+//   palette: {
+//     0.0: '#008800',
+//     0.5: '#ffff00',
+//     1.0: '#ff0000',
+//   },
+//   weight: 5,
+//   outlineColor: '#000000',
+//   outlineWidth: 1,
+// };
+
+export type HotlineDataPoint = [number, number, number];
+export type HotlineOptions = {
+  min?: number;
+  max?: number;
+  palette?: { [key: number]: string }; // key 0->1, value is #rrggbb
+  weight?: number;
+  outlineColor?: string;
+  outlineWidth?: number;
+};
+
+interface IHotlineProps {
+  data: HotlineDataPoint[]; // array of [lat, long, z]
+  options?: HotlineOptions;
+}
+
+const updateOnCanvas = (map: L.Map) => {
+  if (map.options.preferCanvas) {
+    // @ts-ignore
+    map._renderer._update();
+  }
+};
+
+const createLeafletElement = (props: IHotlineProps, context: LeafletContextInterface) => {
+  useEffect(() => {
+    return () => {
+      updateOnCanvas(context.map);
+    };
+  }, []);
+
+  const { data, options = {} } = props;
+
+  const zValues = data.map((dataPoint) => dataPoint[2]);
+  if (!options.min) {
+    options.min = Math.min(...zValues);
+  }
+  if (!options.max) {
+    options.max = Math.max(...zValues);
+  }
+  if (!options.weight) {
+    options.weight = 3;
+  }
+  console.log('hotline options', options);
+  // @ts-ignore
+  const instance = new L.Hotline(data, options);
+  if (context.map.options.preferCanvas) {
+    setTimeout((map: L.Map) => {
+      // Handling react-leaflet bug of canvas renderer not updating
+      // @ts-ignore
+      map._renderer._update();
+    }, 0, context.map);
+  }
+  return { instance, context };
+};
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const updateLeafletElement = (
+  instance: L.Layer,
+  props: IHotlineProps,
+  prevProps: IHotlineProps,
+) => {
+  console.log('updateLeafletElement triggered', props, instance);
+  // @ts-ignore
+  updateOnCanvas(instance._map);
+};
+
+export default createLayerComponent<L.Layer, LayerProps & IHotlineProps>(
+  createLeafletElement,
+  updateLeafletElement,
+);
--- src/components/event/leaflet/TrackWaypoints.tsx
+++ src/components/event/leaflet/TrackWaypoints.tsx
@@ -2,13 +2,21 @@
 import React, { FunctionComponent } from 'react';
 import { Polyline } from 'react-leaflet';
 import { PathOptions } from 'leaflet';
+import Hotline, { HotlineDataPoint } from './Hotline';
 
 import {
   OEventTrack,
+  OEventTrackDetailed,
   OEventTrackPositions,
   OEventWaypoint,
   OEventPosition,
 } from '../../../types/event';
+
+function isDetailedTrack(
+  test: OEventTrackDetailed | OEventTrackPositions,
+): test is OEventTrackDetailed {
+  return test.some((point: OEventWaypoint | OEventPosition) => !Array.isArray(point));
+}
 
 interface Props {
   track: OEventTrack;
@@ -21,15 +29,22 @@ const DEFAULT_PATH_OPTIONS = {
 
 const TrackWaypoints: FunctionComponent<Props> = ({ track, pathOptions }) => {
   if (!track.length) return null;
-  const positions: OEventTrackPositions = track
-    .map((waypoint: OEventWaypoint | OEventPosition) => {
-      if (!Array.isArray(waypoint)) {
-        const coords: OEventPosition = [waypoint.lat, waypoint.long];
-        return coords;
-      }
-      return waypoint;
-    });
-  return <Polyline positions={positions} pathOptions={pathOptions || DEFAULT_PATH_OPTIONS} />;
+  if (isDetailedTrack(track)) {
+    const data: HotlineDataPoint[] = track
+      .map((waypoint) => [waypoint.lat, waypoint.long, waypoint.altitude || 0]);
+    return <Hotline key={data.length} data={data} />;
+  }
+  return <Polyline positions={track} pathOptions={pathOptions || DEFAULT_PATH_OPTIONS} />;
+
+  // const positions: OEventTrackPositions = track
+  //   .map((waypoint: OEventWaypoint | OEventPosition) => {
+  //     if (!Array.isArray(waypoint)) {
+  //       const coords: OEventPosition = [waypoint.lat, waypoint.long];
+  //       return coords;
+  //     }
+  //     return waypoint;
+  //   });
+  // return <Polyline positions={positions} pathOptions={pathOptions || DEFAULT_PATH_OPTIONS} />;
 };
 
 export default TrackWaypoints;
