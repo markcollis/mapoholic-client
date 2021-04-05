ca9dd64d14b4dba8fc053b0308ee074e605cea1e
Mark Collis <markc@salsitasoft.com>
2021-04-05 21:59:35 +0200

    Fix issue with more than one polygon in union

--- src/components/event/leaflet/getPolygonBounds.ts
+++ src/components/event/leaflet/getPolygonBounds.ts
@@ -65,6 +65,59 @@ export const getMapCorners = (event: OEvent | OEventSummary, runnerId: string) =
   return [];
 };
 
+// simple approximation of distance (in metres) for coordinates that are close together
+const calculateDistance = (a: OEventPosition, b: OEventPosition): number => {
+  if (!a || !b) throw new Error('can not calculate distance, invalid positions');
+  const [aLatDegrees, aLongDegrees] = a;
+  const [bLatDegrees, bLongDegrees] = b;
+  if (Math.abs(aLatDegrees) > 90 || Math.abs(bLatDegrees) > 90
+    || Math.abs(aLongDegrees) > 180 || Math.abs(bLongDegrees) > 180) throw new Error('can not calculate distance, invalid lat/long');
+  const degreesToRadians = (degrees: number): number => (degrees * Math.PI) / 180;
+
+  const RADIUS_EARTH = 6371000;
+  const aLat = degreesToRadians(aLatDegrees);
+  const aLong = degreesToRadians(aLongDegrees);
+  const bLat = degreesToRadians(bLatDegrees);
+  const bLong = degreesToRadians(bLongDegrees);
+  const x = (aLong - bLong) * Math.cos((aLat + bLat) / 2);
+  const y = aLat - bLat;
+  return RADIUS_EARTH * Math.sqrt(x * x + y * y);
+};
+
+type NearestPair = {
+  aIndex: number;
+  aPosition: OEventPosition;
+  bIndex: number;
+  bPosition: OEventPosition;
+  distance: number; // metres
+};
+
+const findNearestCoordinatePair = (a: OEventPosition[], b: OEventPosition[]): NearestPair => {
+  if (!a.length || !b.length) throw new Error('Invalid coordinates');
+  const pairs = a.flatMap((aPosition, aIndex) => b.map((bPosition, bIndex) => ({
+    aIndex,
+    aPosition,
+    bIndex,
+    bPosition,
+    distance: calculateDistance(aPosition, bPosition),
+  }))).sort((x, y) => {
+    return x.distance > y.distance ? 1 : -1;
+  });
+  return pairs[0];
+};
+
+const combinePolygons = (a: OEventPosition[], b: OEventPosition[]): OEventPosition[] => {
+  if (!a.length) return b;
+  if (!b.length) return a;
+  const nearestCoordinatePair = findNearestCoordinatePair(a, b);
+  return [
+    ...a.slice(0, nearestCoordinatePair.aIndex + 1),
+    ...b.slice(nearestCoordinatePair.bIndex),
+    ...b.slice(0, nearestCoordinatePair.bIndex + 1),
+    ...a.slice(nearestCoordinatePair.aIndex),
+  ];
+};
+
 // Return appropriate polygon bounds for location maps
 export const derivePolygonBoundsFromEvent = (
   event: OEvent | OEventSummary,
@@ -79,9 +132,6 @@ export const derivePolygonBoundsFromEvent = (
     [corners.se.lat, corners.se.long],
     [corners.sw.lat, corners.sw.long],
   ]);
-  const union = polygon.union(...mapCornerPositions);
-  // console.log('union', union);
-  return union[0];
-  // if they don't overlap union will have multiple elements
-  // deal with this later...
+  const union: OEventPosition[][] = polygon.union(...mapCornerPositions);
+  return union.reduce((acc, val) => combinePolygons(acc, val), []);
 };
